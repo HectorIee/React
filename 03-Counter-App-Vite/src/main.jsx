255a3002cf8af38bd832c9d117a22ae0ace237f0
HectorIee <hector1vazquez999@gmail.com>
2023-11-04 13:10:06 -0600

    Part2

--- 03-Counter-App-Vite/src/main.jsx
+++ 03-Counter-App-Vite/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import {createRoot} from "react-dom/client"; // llamada al React DOM para renderizar componentes
+import HelloWorld from "./components/HelloWorld"; './components/HelloWorld'
+ 
+// react 18
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <HelloWorld />
+    </React.StrictMode>
+);
