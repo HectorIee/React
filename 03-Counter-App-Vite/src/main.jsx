241c246c0cf02d09855a88741800932488e5e996
HectorIee <hector1vazquez999@gmail.com>
2023-11-11 12:37:20 -0600

    pt2 first

--- 03-Counter-App-Vite/src/main.jsx
+++ 03-Counter-App-Vite/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {createRoot} from "react-dom/client"; // llamada al React DOM para renderizar componentes
-import HelloWorld from "./components/HelloWorld"; './components/HelloWorld'
+import {FirstApp} from "./components/FirstApp";
+
+import './styles.css'
  
 // react 18
 const root = createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HelloWorld />
+        <FirstApp />
     </React.StrictMode>
 );
