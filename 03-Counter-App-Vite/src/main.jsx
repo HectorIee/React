f6e416d99ca50cfc8968f0e116917fe528ff488f
HectorIee <hector1vazquez999@gmail.com>
2023-11-13 19:09:59 -0600

    pt3

--- 03-Counter-App-Vite/src/main.jsx
+++ 03-Counter-App-Vite/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client"; // llamada al React DOM para renderizar componentes
 import {FirstApp} from "./components/FirstApp";
+import {CounterApp} from "./components/CounterApp";
 
 import './styles.css'
  
@@ -8,6 +9,6 @@ import './styles.css'
 const root = createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={10} />
     </React.StrictMode>
 );
