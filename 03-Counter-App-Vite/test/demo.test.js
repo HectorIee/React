b0f14fdbf115d44c1032ea7ebf8d2b31de091a08
HectorIee <hector1vazquez999@gmail.com>
2023-11-29 18:38:11 -0600

    Pruebas con jest .toBe y configuracion para que se muestren atajos

--- 03-Counter-App-Vite/test/demo.test.js
+++ 03-Counter-App-Vite/test/demo.test.js
@@ -1,7 +1,19 @@
 
-test('Esta prueba no debe de fallar', () => {
-    
-    if(1 == 0){
-        throw new Error('No puede dividir entre 0')
-    }
+describe('Pruebas en <DemoComponent>', () => {
+
+    test('Esta prueba no debe de fallar', () => {
+        // 1. Inicialización
+        const message1 = 'Hola Mundo'
+
+        // 2. Estimulo
+        const message2 = message1.trim()
+
+        // 3. Observaciones del comportamiento .... esperado
+        // expect( message1 ).toBe( message2 )
+        expect(message1).toBe(message2)
+    })
+
 })
+
+
+
