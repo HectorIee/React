04db813eb312e9b9180ede81d6c3b9e349ae3e7e
HectorIee <hector1vazquez999@gmail.com>
2024-01-13 12:38:18 -0600

    addCategory

--- 04-Gif-Expert-App/src/GifExpertApp.jsx
+++ 04-Gif-Expert-App/src/GifExpertApp.jsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball', 'Jujutsu Kaisen']);
-    console.log(categories)
+
+    const onAddCategory = () => {
+        setCategories( [ ...categories, 'Valorant' ])
+    }
 
     return (
         <>
@@ -14,6 +17,7 @@ const GifExpertApp = () => {
             {/* Input */}
 
             {/* Listado de Gifs */}
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map((category) => (
                     <li key={category}> {category} </li>
